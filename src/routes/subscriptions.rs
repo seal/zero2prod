0ebfca92965b1be978101513d285ff90a717d17b
seal <will@kimbell.uk>
2024-04-11 14:41:23 +0100

    End of chapt 4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,7 +12,7 @@ pub struct FormData {
     name = "Adding a new subscriber",
     skip(form, pool),
     fields(
-        request_id = %Uuid::new_v4(),
+        //request_id = %Uuid::new_v4(), different from the actix one 
         subscriber_email = %form.email,
         subscriber_name = %form.name,
     )
